6d74675f9764519952e2f44b55871045ebf0b442
EvelinSA05 <evelinsalsabila27@gmail.com>
2022-11-16 08:41:53 +0700

    codeigniter4 bag 1

---
+++ codeigniter4-bag-1/restoran-ci4/ci4/app/Controllers/Kategori.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Controllers;
+
+class Kategori extends BaseController
+{
+    public function index()
+    {
+        // return view('welcome_message');
+        echo "<h1>belajar ci4</h1>";
+    }
+}
