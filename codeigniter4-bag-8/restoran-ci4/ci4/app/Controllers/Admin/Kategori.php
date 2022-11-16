8a455477a581fe36bd0e34677f596e8dc9e51245
EvelinSA05 <evelinsalsabila27@gmail.com>
2022-11-16 08:54:08 +0700

    codeigniter4 bag 8

---
+++ codeigniter4-bag-8/restoran-ci4/ci4/app/Controllers/Admin/Kategori.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Controllers\Admin;
+
+use App\Controllers\BaseController;
+
+class Kategori extends BaseController
+{
+    public function index()
+    {
+        // return view('welcome_message');
+        echo "<h1>belajar ci4</h1>";
+    }
+
+    public function select()
+    {
+        $data = [
+            'judul'=>'SELECT DATA DARI controller',
+            'kategori'=>['Makanan', 'Minuman', 'Jajan', 'Gorengan', 'Buah']
+        ];
+
+        return view("kategori/select",$data);
+    }
+
+    public function selectWhere($id = null)
+    {
+        echo "<h1>menampilkan data yang dipilih $id</h1>";
+    }
+
+    public function formInsert()
+    {
+        return view("kategori/forminsert");
+    }
+
+    public function formUpdate($id=null)
+    {
+        echo view("template/header");
+        echo view("kategori/update");
+        echo view("template/footer");
+    }
+
+    public function update($id = null)
+    {
+        echo "proses update data $id";
+    }
+
+    public function delete($id = null)
+    {
+        echo "proses delete data";
+    }
+}
---
+++ codeigniter4-bag-8/restoran-ci4/ci4/app/Views/kategori/select.php
@@ -0,0 +1,13 @@
+<?= $this->extend('template/admin') ?>
+
+<?= $this->section('content') ?>
+
+<h1><?= $judul; ?></h1>
+
+<?php foreach($kategori as $key => $value): ?>
+<h2><?= $value ?></h2>
+<?php endforeach; ?>
+
+<h1><?= $kategori[1] ?></h1>
+
+<?= $this->endSection() ?>
